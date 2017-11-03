c06f89f8a9b16208e5e2a5952033f49d61dc2d26
rihabkallel <rkallel@hrdatabank.com>
2017-11-03 10:06:55 +0100

    webhook

--- src/main/java/com/example/controller/BotController.java
+++ src/main/java/com/example/controller/BotController.java
@@ -33,7 +33,9 @@ public class BotController {
 	private @ResponseBody Map<String, Object> webhook(@RequestBody Map<String, Object> obj)
 			throws JSONException, IOException {
 
-		String channelToken = "CqLM9SJd77pSTss73OSXTNJZFs+vajIE2YZtsMqWMWN6NY+9FlFdNr4+FI9If8CRzU2xCX/E+XbGSKhGZnQTeGBf/oJIugROLO1n85hnQVSUp3X7bUxCJVCGi+l/lp9fcobhuwFeKn9vr2VMuIpSKwdB04t89/1O/w1cDnyilFU=";
+		String channelToken = "wvydTwaiKtsG4Z90XPfG6hWB31/TX2tceTz+v1NqSXgOMgUZ55c4GnZZ6rd+i9lJn8d0k17/7A5E0Mq1kKpmAdMKWkmqGaiezxDAZykxJIA8MoDYx+a19t4cQbRd5zLWl3k30y2pSM1zzZQz/JVSjwdB04t89/1O/w1cDnyilFU=";
+
+		System.out.println("*****************WEBHOOK*********************");
 
 		Map<String, Object> json = new HashMap<String, Object>();
 
@@ -53,88 +55,8 @@ public class BotController {
 		JSONObject fulfillment = result.getJSONObject("fulfillment");
 		String speech = fulfillment.getString("speech");
 
-		java.util.List<String> sampleLinksOsaka = new ArrayList<>();
-		java.util.List<String> sampleLinksTokyo = new ArrayList<>();
-
-		java.util.List<String> randomLinksOsaka = new ArrayList<>();
-		java.util.List<String> randomLinksTokyo = new ArrayList<>();
-
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000115892.html");
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000016010.html");
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000075865.html");
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000007615.html");
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000007413.html");
-		sampleLinksOsaka.add("http://www.cjs.ne.jp/detail_b/T0000255552.html");
-
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000430079.html");
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000394280.html");
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000282562.html");
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000301712.html");
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000245299.html");
-		sampleLinksTokyo.add("http://www.cjs.ne.jp/detail_b/T0000242702.html");
-
-		Collections.shuffle(sampleLinksOsaka);
-		Collections.shuffle(sampleLinksTokyo);
-
-		for (int i = 0; i < 5; i++) {
-			randomLinksOsaka.add(sampleLinksOsaka.get(i));
-		}
-
-		for (int i = 0; i < 5; i++) {
-			randomLinksTokyo.add(sampleLinksTokyo.get(i));
-		}
-
-		if (!intentName.equals("Default Fallback Intent")) {
-			if (customerMessage.toLowerCase().equals("osaka")
-					|| (parameters != null && parameters.getString("japan-cities").toLowerCase().equals("osaka"))) {
-				carouselForUser(userId, channelToken, randomLinksOsaka);
-			} else if (customerMessage.toLowerCase().equals("tokyo")
-					|| (parameters != null && parameters.getString("japan-cities").toLowerCase().equals("tokyo"))) {
-				carouselForUser(userId, channelToken, randomLinksTokyo);
-			}
-		}
-
 		return json;
 
 	}
 
-	/**
-	 * Method for send carousel template message to use
-	 * 
-	 * @param userId
-	 * @param lChannelAccessToken
-	 * @param nameSatff1
-	 * @param nameSatff2
-	 * @param poster1_url
-	 * @param poster2_url
-	 * @throws IOException
-	 */
-	private void carouselForUser(String userId, String lChannelAccessToken, java.util.List<String> randomLinks)
-			throws IOException {
-
-		java.util.List<CarouselColumn> columns = new ArrayList<>();
-
-		for (String link : randomLinks) {
-			Document doc = Jsoup.connect(link).get();
-			String title = doc.getElementsByClass("tit_articleName").get(0).text();
-			String img = doc.getElementsByClass("max-width-260").get(0).attr("abs:src");
-
-			CarouselColumn column = new CarouselColumn(img, title, "Click check for more details",
-					Arrays.asList(new URIAction("check", link)));
-			columns.add(column);
-		}
-
-		CarouselTemplate carouselTemplate = new CarouselTemplate(columns);
-
-		TemplateMessage templateMessage = new TemplateMessage("Your search result", carouselTemplate);
-		PushMessage pushMessage = new PushMessage(userId, templateMessage);
-		try {
-			Response<BotApiResponse> response = LineMessagingServiceBuilder.create(lChannelAccessToken).build()
-					.pushMessage(pushMessage).execute();
-			System.out.println(response.code() + " " + response.message());
-		} catch (IOException e) {
-			System.out.println("Exception is raised ");
-			e.printStackTrace();
-		}
-	}
 }
