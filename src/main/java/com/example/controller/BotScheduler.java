025c509ad83c526ca436d9947f9f125542510adc
rihabkallel <rkallel@hrdatabank.com>
2017-11-10 17:07:22 +0100

    intervie

--- src/main/java/com/example/controller/BotScheduler.java
+++ src/main/java/com/example/controller/BotScheduler.java
@@ -173,7 +173,7 @@ public class BotScheduler {
 	 * 
 	 * @throws Exception
 	 */
-	@Scheduled(cron = "0 07 17 * * *")
+	@Scheduled(cron = "0 09 * * * *")
 	public void sendInterviewTimeMessage() throws Exception {
 
 		System.out.println("************INTERVIEW*******************");
